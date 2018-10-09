0f88f77ebd31c5826d2208eec438a8689143d894
qianzd <qian.wolf@gmail.com>
2018-10-09 16:26:44 +0800

    bak

--- utilDemo/app/src/main/java/com/example/qzd/utildemo/MainActivity.java
+++ utilDemo/app/src/main/java/com/example/qzd/utildemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.qzd.utildemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
